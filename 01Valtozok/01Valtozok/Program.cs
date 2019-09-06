cbda5f2f0c73de767018c85b47f02581543de817
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2019-09-06 13:01:41 +0200

    Változók
    Érték és referencia típusok

---
+++ 01Valtozok/01Valtozok/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01Valtozok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Szöveg
+            string szoveg = "Valami szöveg";
+            //karakter
+            char karakter = 'g';
+            //egész számok
+            int szam = 345;
+            short masikSzam = 235;
+            byte bajt = 221;
+            //logikai
+            bool logikai = false;
+
+            //lebegőpontos számok
+            float egyszeres = 15.6634249364646463462f;
+            double ketszeres = 15.6634249364646463462;
+            decimal dec = 15.6634249364646463462m;
+
+            Console.WriteLine(egyszeres);
+            Console.WriteLine(ketszeres);
+            Console.WriteLine(dec);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ 01Valtozok/01Valtozok/valtozok.md
@@ -0,0 +1,23 @@
+﻿# Változók
+
+A memória egy adott része, amely egy névvel van azonosítva.
+
+## Érték típusok
+
+- egész számok
+- nem egész számok
+- szövegek
+- karakterek
+- logikai értékek
+
+### Nem egész típusok
+- float
+- double
+- decimal
+
+**Pénzügyi alkalmazásokhoz kell a decimal, a többi esetben megfelelő a double, vagy a float**
+
+## Referencia típusú változók
+
+ - tömbök
+ - objektumok stb.
