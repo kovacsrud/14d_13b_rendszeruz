f68b0515547ce375cbb585d84eba0d42222f6181
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2019-09-20 13:09:43 +0200

    Listák

---
+++ Listak/Listak/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Lista: generikus adattípus, bármilyen típusú elemet tartalmazhat
+            List<string> nevek = new List<string>();
+
+            nevek.Add("Zénó");
+            nevek.Add("Ubul");
+            nevek.Add("Lajos");
+            nevek.Add("Ignác");
+
+
+
+            Console.WriteLine(nevek.Count);
+            Console.WriteLine(nevek[0]);
+
+            //Bejárás, listázás for-al
+            for (int i = 0; i < nevek.Count; i++)
+            {
+                Console.WriteLine(nevek[i]);
+            }
+
+            //a lista elemei nem módosíthatóak foreach ciklussal!
+            foreach (var n in nevek)
+            {
+                Console.WriteLine(n);
+            }
+
+            Console.WriteLine(nevek.Contains("Zénó"));
+            nevek.Clear();
+
+
+            Console.ReadKey();
+        }
+    }
+}
