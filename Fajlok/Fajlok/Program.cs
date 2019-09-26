f3cae9e76bc2a3840656a6072a7197815cbd0325
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2019-09-26 13:01:14 +0200

    Fájlok kezelése

--- Fajlok/Fajlok/Program.cs
+++ Fajlok/Fajlok/Program.cs
@@ -9,8 +9,18 @@ namespace Fajlok
 {
     class Program
     {
+        struct Ember
+        {
+            public string vezeteknev;
+            public string keresztnev;
+            public int szuletesiEv;
+            public string szuletesiHely;
+        }
         static void Main(string[] args)
         {
+            Ember ember = new Ember();
+            List<Ember> emberek = new List<Ember>();
+
             try
             {
                 FileStream fajl = new FileStream(@"d:/rud/tesztadat_20k.txt",FileMode.Open);
@@ -20,7 +30,16 @@ namespace Fajlok
                 {
                     while (!sr.EndOfStream)
                     {
-                        Console.WriteLine(sr.ReadLine());
+                        //a fájl egy sorának feldolgozása tömbbe
+                        var e = sr.ReadLine().Split(',');
+                        ember.vezeteknev = e[0];
+                        ember.keresztnev = e[1];
+                        //konvertálni kell számra
+                        ember.szuletesiEv = Convert.ToInt32(e[2]);
+                        ember.szuletesiHely = e[3];
+
+                        emberek.Add(ember);
+                           
                     }
                 }
 
@@ -30,6 +49,37 @@ namespace Fajlok
 
                 Console.WriteLine(ex.Message);
             }
+
+            Console.WriteLine($"Adatsorok száma:{emberek.Count}");
+
+            var hatvankilenc = emberek.FindAll(x=>x.szuletesiEv==1969);
+            //adott feltételnek megfelelő adatok megszámolása
+            var mennyi = emberek.Count(x=>x.szuletesiEv==1969);
+
+            Console.WriteLine(hatvankilenc.Count);
+
+            foreach (var h in hatvankilenc)
+            {
+                Console.WriteLine($"{h.vezeteknev},{h.keresztnev},{h.szuletesiEv},{h.szuletesiHely}");
+            }
+
+            try
+            {
+                FileStream outFajl = new FileStream(@"d:/h9.txt",FileMode.Create);
+                using (StreamWriter sw=new StreamWriter(outFajl,Encoding.Default))
+                {
+                    foreach (var h in hatvankilenc)
+                    {
+                        sw.WriteLine($"{h.vezeteknev},{h.keresztnev},{h.szuletesiEv},{h.szuletesiHely}");
+                    }
+                }
+                Console.WriteLine("Kiírás kész!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
 
             Console.ReadKey();
         }
--- Fajlok/Fajlok/fajlok.md
+++ Fajlok/Fajlok/fajlok.md
@@ -53,3 +53,64 @@ try
                 Console.WriteLine(ex.Message);
             }
 ```
+A fájl adatainak a feldolgozásához struktúrát, vagy osztályt célszerű használni.
+
+**Struktúra deklaráció**
+
+```C#
+struct Ember
+        {
+            public string vezeteknev;
+            public string keresztnev;
+            public int szuletesiEv;
+            public string szuletesiHely;
+        }
+```
+
+A fájl sorainak feldolgozása a cikluson belül:
+```C#
+using (StreamReader sr = new StreamReader(fajl, Encoding.Default))
+    {
+        while (!sr.EndOfStream)
+            {
+             //a fájl egy sorának feldolgozása tömbbe
+            var e = sr.ReadLine().Split(',');
+            ember.vezeteknev = e[0];
+            ember.keresztnev = e[1];
+            //konvertálni kell számra
+            ember.szuletesiEv = Convert.ToInt32(e[2]);
+            ember.szuletesiHely = e[3];
+
+            emberek.Add(ember);
+                           
+             }
+   }
+```
+
+**Adott feltételnek megfelelő adatok leválogatása egy új listába**
+
+```C#
+var hatvankilenc = emberek.FindAll(x=>x.szuletesiEv==1969);
+```
+
+**A kiválogatott adatok fájlba írása**
+
+```C#
+try
+    {
+        FileStream outFajl = new FileStream(@"d:/h9.txt",FileMode.Create);
+        using (StreamWriter sw=new StreamWriter(outFajl,Encoding.Default))
+            {
+                    foreach (var h in hatvankilenc)
+                    {
+                        sw.WriteLine($"{h.vezeteknev},{h.keresztnev},{h.szuletesiEv},{h.szuletesiHely}");
+                    }
+            }
+                Console.WriteLine("Kiírás kész!");
+            }
+catch (Exception ex)
+    {
+                Console.WriteLine(ex.Message);
+    }
+
+```
