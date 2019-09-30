5ef01d140d03bf2133f5376051764e69c5ec746f
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2019-09-30 11:10:15 +0200

    Fájl beolvasás más parancsokkal

---
+++ Fajlok/FajlokAllread/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FajlokAllread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Fájlok beolvasása egyéb módokon
+            try
+            {
+                
+                var sorok = File.ReadAllLines(@"d:/rud/tesztadat_20k.txt", Encoding.Default);
+                
+                
+
+                for (int i = 0; i < sorok.Length; i++)
+                {
+                    //Itt dolgozzuk fel a tömb adatait
+                    Console.WriteLine(sorok[i]);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);                
+            }
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
