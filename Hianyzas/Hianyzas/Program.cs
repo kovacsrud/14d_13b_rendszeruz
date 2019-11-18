0a42fcde9ed76dc5a057a9ef39bc928bd060f14c
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2019-11-18 09:56:07 +0100

    Hianyzasok feladat, struktúra és beolvasás

---
+++ Hianyzas/Hianyzas/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hianyzas
+{
+    class Program
+    {
+        struct Hianyzas
+        {
+            string nev;
+            string osztaly;
+            int elsoNap;
+            int utolsoNap;
+            int mulasztottOrak;
+
+            public Hianyzas(string adatsor)
+            {
+                var e = adatsor.Split(';');
+                nev = e[0];
+                osztaly = e[1];
+                elsoNap = Convert.ToInt32(e[2]);
+                utolsoNap = Convert.ToInt32(e[3]);
+                mulasztottOrak = Convert.ToInt32(e[4]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            List<Hianyzas> hianyzasok = new List<Hianyzas>();
+
+            try
+            {
+                var sorok = File.ReadAllLines(@"c:/ftproot/Hianyzasok/szeptember.csv",Encoding.Default);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
